7be3328478fcbe49bf99fe1e1c9ec86854153109
Fabricio <fabricio.altamirano@exaccta.com>
2019-04-10 23:55:15 +0200

    Add html

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const http = require('http');
 const express = require('express');
 const app  = express();
+const io = require('socket.io');
+
 const server = http.createServer(app);
 
 app.set('port',process.env.PORT || 8080);
-//app.set('port',3000);
+
 app.use(express.static(__dirname+"/public"));
 
 
 server.listen(app.get('port'), function(){
-console.log("Hola cara cola");
+console.log("Servidor iniciado");
 
 });
+
+var socket = io.listen(server);
+
+socket.on('connection', function(socket){
+ console.log('cliente conectado');
+})
 
 
 /*http.createServer(function (req, res) {
