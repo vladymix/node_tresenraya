a819e8a70f2b79081b6771e47e5f47763f130301
Fabricio <fabricio.altamirano@exaccta.com>
2019-04-10 23:34:20 +0200

    change server js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var http = require('http');
+const http = require('http');
+const express = require('express');
+const app  = express();
+const server = http.createServer(app);
 
-http.createServer(function (req, res) {
-    
+app.set('port',process.env.PORT || 8080);
+app.use(express.static(__dirname+"/public"));
+
+server.listen(app.get('port'), function(){
+console.log("Hola cara cola");
+
+});
+
+/*http.createServer(function (req, res) {
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('PRUEBA DESDE GIT HUB! by vlady');
     
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 8080);*/
